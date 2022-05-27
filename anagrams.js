75b7a136b562c3cc64ba1113f727b3bb46dc8f26
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-05-27 23:42:53 +0200

    continue to struggle figuring out how to identify the word with the same characters in the array

--- anagrams.js
+++ anagrams.js
@@ -1,12 +1,26 @@
 function anagrams(word, words) {
-    let result = [];
-    for (let n of words){
-        if (n.includes(word.split(""))){
-            return result.push(n);
-        } else{
-            return result;
+    let outcome = [];
+    let equalChars = [];
+    let referenceWord = word.split("");
+
+
+     words.map(word => {
+         let endWord = word.split("");
+        if(referenceWord.length === endWord.length){
+            let i = 0;
+            while(i < referenceWord.length){
+                if(referenceWord[i] === endWord[i]){
+                    outcome.push(endWord.join(""));
+                };
+                i++;
+            };
+            
         }
-    }
+        
+    })
+    
+    // return equalChars;
+    
 }
 
 console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']));
