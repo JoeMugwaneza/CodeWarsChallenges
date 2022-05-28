5d7676434301a2ebc1f6e0bd660494577c154cb0
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-05-28 20:21:52 +0200

    added description of challenge and removed unnecessary comments

--- anagrams.js
+++ anagrams.js
@@ -1,24 +1,44 @@
+// What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+// 'abba' & 'baab' == true
+
+// 'abba' & 'bbaa' == true
+
+// 'abba' & 'abbba' == false
+
+// 'abba' & 'abca' == false
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+// Note for Go
+// For Go: Empty string slice is expected when there are no anagrams found.
+
 function anagrams(word, words) {
-    let outcome = [];
-    // let equalChars = [];
     let referenceWord = word.split("").sort().join("");
     const anagramsIndexes = [];
-
+    
     let newWords = [...words.map(word => word.split("").sort().join(""))]
     for(let i = 0; i < newWords.length; i++){
         if(referenceWord === newWords[i]){
             anagramsIndexes.push(i)
-
+            
         }
     }
+
+    let anagramsOutcome = [];
+
     for( let i = 0; i < words.length; i++){
         for(let j = 0; j < anagramsIndexes.length; j++){
             if(i === j){
-                outcome.push(words[anagramsIndexes[j]])
+                anagramsOutcome.push(words[anagramsIndexes[j]])
             }
         }
     }
-    return outcome;
+    return anagramsOutcome;
 }
 
 console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']));
