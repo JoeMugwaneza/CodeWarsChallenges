4ca757079eef012285e284066579b7aafe3d4228
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-01 22:34:55 +0200

    worked in knowing if a number is a prime or not

--- isPrime.js
+++ isPrime.js
@@ -1,4 +1,11 @@
-function isPrime(num) {
-    //TODO
+function isPrime(num){
+  let numSq =Math.sqrt(num);
+  for (let i = 2; i <= numSq; i++){
+    if( num % i === 0) return false;
   }
+  return num > 1;
+
+}
+
   
+  console.log(isPrime(29));
