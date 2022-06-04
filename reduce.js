31233f3707e1109ec0e7d56d443be04d7db806b0
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-04 08:30:30 +0200

    continued to practice the use of reduce method in the array manipulation

--- reduce.js
+++ reduce.js
@@ -6,4 +6,20 @@ let returnedArray = arrayOfArrays.reduce((a,b) =>{
     return a.concat(b);
 },[])
 
-console.log(returnedArray);
+// console.log(returnedArray);
+
+
+// Counting instances of values in an object
+
+const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+let countedNames = names.reduce(function(allNames, name){
+    if(name in allNames) {
+        allNames[name]++
+    } else{
+
+        allNames[name] = 1 
+    }
+    return allNames
+},{})
+
+console.log(countedNames);
