fb63e4df48e2bcd5045248a7395320afe6e9a931
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-04 08:19:12 +0200

    worked readability of codes

--- reduce.js
+++ reduce.js
@@ -1,7 +1,9 @@
 // bring one single array from a array of arrays
 
-let arrayOfArrays = [[0, 1], [2, 3], [4, 5]].reduce((a,b) =>{
+let arrayOfArrays = [[0, 1], [2, 3], [4, 5]]
+
+let returnedArray = arrayOfArrays.reduce((a,b) =>{
     return a.concat(b);
 },[])
 
-console.log(arrayOfArrays);
+console.log(returnedArray);
